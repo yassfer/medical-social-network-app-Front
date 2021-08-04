007fc678476e3b5948beb4ba5622cb5d85871aa1
unknown <ferchichi.yasmine@yahoo.com>
2021-08-04 23:19:40 +0100

    first commit

---
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { AdminLayoutRoutes } from "./admin-layout.routing";
+import { ChallengeComponent } from "../../pages/challenge/challenge.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+// import { RtlComponent } from "../../pages/rtl/rtl.component";
+
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+  ],
+  declarations: [
+    ChallengeComponent,
+    UserComponent,
+    TablesComponent,
+    IconsComponent,
+    TypographyComponent,
+    NotificationsComponent,
+    // RtlComponent
+  ]
+})
+export class AdminLayoutModule {}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,19 @@
+import { Routes } from "@angular/router";
+
+import { ChallengeComponent } from "../../pages/challenge/challenge.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+// import { RtlComponent } from "../../pages/rtl/rtl.component";
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "challenge", component: ChallengeComponent },
+  { path: "icons", component: IconsComponent },
+  { path: "notifications", component: NotificationsComponent },
+  { path: "user", component: UserComponent },
+  { path: "tables", component: TablesComponent },
+  { path: "typography", component: TypographyComponent },
+  // { path: "rtl", component: RtlComponent }
+];
